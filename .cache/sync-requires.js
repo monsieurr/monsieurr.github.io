482643765af68f13f955dbfd6c6bf98491423dca
monsieurr <plouftoto@gmail.com>
2020-07-06 19:02:58 +0200

    test

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/thomaslefebvre/git/otium/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/thomaslefebvre/git/otium/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/thomaslefebvre/git/otium/src/pages/index.js"))),
   "component---src-pages-page-2-js": hot(preferDefault(require("/Users/thomaslefebvre/git/otium/src/pages/page-2.js"))),
